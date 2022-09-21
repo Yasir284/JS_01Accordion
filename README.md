ae9263b173b57a5c4d0f876d974a65b56391fdf0
Yasir284 <lambawalay@gmail.com>
2022-09-21 17:55:20 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,43 @@
+![badge](https://img.shields.io/badge/MADE%20WITH-HTML,%20CSS%20&%20JS-blue)
+![badge](https://img.shields.io/badge/TIME%20TAKEN-3-4%20hrs-red)
+[![badge](https://img.shields.io/badge/SEE%20DEMO%20-VISIT-green)]()
+
+# JavaScript Accordion
+
+- This is my first Javascript mini-Project. In this project I have made an FAQ section with JavasCript, in which you can see the answer of the FAQ's on clicking the '+' button.
+
+- I have also added some animation to the button and the answer section
+
+- It took me 3-4 hrs to make this project.
+
+  [![badge](https://img.shields.io/badge/LINK%20OF-PROJECT-darkgreen)]()
+
+---
+
+## 🛠 Skills learned
+
+- JavaScript
+- To Use DOM
+- Creating Elements in Javascript
+- Adding Elements in HTML using JavaScript
+- Some animations in CSS
+
+---
+
+## 🎥 Working
+
+![Gif](./Js_project1.gif)
+
+---
+
+## 🚀 About Me
+
+### Hey, I am Yasir lambawala
+
+- I am born and brought up in Vadodara, Gujarat. I have done my Btech at GTU university and I am currently learning Web-development.
+
+---
+
+## Feedback
+
+- If you have any feedback, please reach me at lambawalay@gmail.com
---
+++ faq.js
@@ -0,0 +1,79 @@
+const faqData = [
+  {
+    id: 1,
+    question: "Who are we?",
+    answer:
+      "We enable upscaling careers through flexible, interactive and collaborative learning. We believe in building learning communities by bringing together mentors, young minds, and creators.",
+  },
+  {
+    id: 2,
+    question: "What do we do?",
+    answer:
+      "Building learning communities with Our Affordable & Competent Courses",
+  },
+  {
+    id: 3,
+    question: "Are the community classes boring?",
+    answer: "No, not at all!!!",
+  },
+];
+
+const accordianBody = document.querySelector(".accordian_body");
+
+// Creating elements of faq and adding eventListner to the button
+function createFaq(questionTxt, answerTxt) {
+  let mainDiv = document.createElement("div");
+  let headerDiv = document.createElement("div");
+  let question = document.createElement("h3");
+  let answer = document.createElement("p");
+  let btn = document.createElement("button");
+  mainDiv.setAttribute("class", "faq");
+  headerDiv.setAttribute("class", "faq_header");
+  answer.setAttribute("class", "hidden");
+  btn.setAttribute("class", "show_btn");
+  btn.textContent = "+";
+  accordianBody.appendChild(mainDiv);
+  mainDiv.appendChild(headerDiv);
+  headerDiv.appendChild(question);
+  mainDiv.appendChild(answer);
+  headerDiv.appendChild(btn);
+
+  question.textContent = questionTxt;
+  answer.textContent = answerTxt;
+
+  // button click event
+  btn.addEventListener("click", btnStatusUpdate);
+  function btnStatusUpdate() {
+    btn.classList.toggle("rotate");
+
+    // ====== Optimized Logic ========
+    setTimeout(() => {
+      answer.classList.toggle("hidden", !answer.classList.contains("hidden"));
+    }, 300);
+    answer.classList.toggle("opacityIn", !answer.classList.contains("hidden"));
+    answer.classList.toggle("opacityOut", answer.classList.contains("hidden"));
+  }
+
+  // ====== Unoptimized Logic =========
+  // if (answer.classList.contains("hidden")) {
+  //   setTimeout(() => {
+  //     answer.classList.remove("hidden");
+  //   }, 300);
+  //   answer.classList.remove("opacityIn");
+  //   answer.classList.add("opacityOut");
+  // } else {
+  //   answer.classList.remove("opacityOut");
+  //   answer.classList.add("opacityIn");
+  //   setTimeout(() => {
+  //     answer.classList.add("hidden");
+  //   }, 300);
+  // }
+}
+
+// Adding Faq to the page
+function showFaq() {
+  for (let i = 0; i < faqData.length; i++) {
+    createFaq(faqData[i].question, faqData[i].answer);
+  }
+}
+showFaq();
